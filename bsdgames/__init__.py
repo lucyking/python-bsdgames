4f80ad2389059e13e1508cc42aeef5cac8c8111c
lucyking <lucyking@users.noreply.github.com>
2016-01-31 22:38:19 +0800

    Update __init__.py

--- bsdgames/__init__.py
+++ bsdgames/__init__.py
@@ -1,7 +1,9 @@
 import _pom
+
 def main():
     """Entry point for the application script"""
     print("Call your main application code here")
 def pom():
+    """call Pom() from pom module"""
     _pom.Pom()
 
