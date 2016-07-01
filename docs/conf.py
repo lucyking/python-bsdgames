ca02db15e57e5b25e7a2205078393ce6db14b294
lucyking <joehisaishi1943@gmail.com>
2016-07-01 18:48:02 +0800

    comment theme
    Signed-off-by: lucyking <joehisaishi1943@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
@@ -122,8 +122,8 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'sphinx-rtd-theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_theme = 'sphinx-rtd-theme'
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
